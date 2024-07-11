ee3d9aa36bba7e83f7fbc9faf578c4a783e330d9
Serhii-Korsh <167204860+Serhii-Korsh@users.noreply.github.com>
2024-07-11 12:59:55 +0300

    changes index file

--- js/task-3.js
+++ js/task-3.js
@@ -6,4 +6,3 @@ return `${boxSizing}`
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-console.log(typeof typeof 1);
